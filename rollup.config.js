2f77b457e51a135897b605fcf72447642df30b53
hiro08gh <hiro08gh@gmail.com>
2021-05-10 15:01:21 +0900

    chore: changed build tool from webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import pluginNodeResolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: './src/index.ts',
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true,
+    }),
+  ],
+  output: [
+    {
+      file: `./dist/cjs/${pkg.name}.js`,
+      sourcemap: 'inline',
+      format: 'cjs',
+      plugins: [terser()],
+    },
+    {
+      file: `./dist/esm/${pkg.name}.js`,
+      sourcemap: 'inline',
+      format: 'esm',
+      plugins: [terser()],
+    },
+    {
+      file: `./dist/umd/${pkg.name}.js`,
+      sourcemap: 'inline',
+      format: 'umd',
+      name: 'microcms',
+      globals: { 'node-fetch': 'fetch', 'query-string': 'qs' },
+      plugins: [
+        terser(),
+        pluginNodeResolve({
+          borwser: true,
+        }),
+      ],
+    },
+  ],
+  external: ['node-fetch', 'query-string'],
+};
