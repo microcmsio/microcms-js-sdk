11bddae99640a3ea0a903791b382b4dcb62e3562
dc7290 <dhkh.cba0927@gmail.com>
2022-08-02 19:02:19 +0900

    typescriptのeslint設定の優先度を上げる 型定義のところにもun-defルールとかが適用されてしまうのを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'eslint:recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
