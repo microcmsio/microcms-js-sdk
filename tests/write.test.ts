d553f3454aacb601193f49d8de476c24bc24079f
dc7290 <dhkh.cba0927@gmail.com>
2022-08-03 18:52:46 +0900

    WRITE APIについてのテストリストを作成

---
+++ tests/write.test.ts
@@ -0,0 +1,18 @@
+describe('create', () => {
+  test.todo('List format content can be posted');
+  test.todo('Object type content can be posted');
+
+  test.todo('Returns an error message if `endpoint` is not specified');
+});
+describe('update', () => {
+  test.todo('List format content can be updated');
+  test.todo('Object type content can be updated');
+
+  test.todo('Returns an error message if `endpoint` is not specified');
+});
+describe('delete', () => {
+  test.todo('List format content can be deleted');
+
+  test.todo('Returns an error message if `endpoint` is not specified');
+  test.todo('Returns an error message if `contentId` is not specified');
+});
