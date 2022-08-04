7f4f4ee5d4493cc3e0ea370b9d13b83b12243159
dc7290 <dhkh.cba0927@gmail.com>
2022-08-04 13:48:53 +0900

    create関数に対してのテストする内容を記述

--- tests/write.test.ts
+++ tests/write.test.ts
@@ -1,10 +1,103 @@
-describe('create', () => {
-  test.todo('Content can be submitted without specifying an id');
-  test.todo('Draft content can be submitted by specifying an id');
-  test.todo('Content can be submitted by specifying an id');
-  test.todo('Draft content can be submitted by specifying an id');
+import { rest } from 'msw';
 
-  test.todo('Returns an error message if `endpoint` is not specified');
+import { createClient } from '../src/createClient';
+
+import { testBaseUrl } from './mocks/handlers';
+import { server } from './mocks/server';
+
+const client = createClient({
+  serviceDomain: 'serviceDomain',
+  apiKey: 'apiKey',
+});
+
+interface ContentType {
+  title: string;
+  body?: string;
+}
+
+describe('create', () => {
+  const postApiMockFn = jest.fn();
+  const putApiMockFn = jest.fn();
+
+  beforeEach(() => {
+    server.use(
+      rest.post(`${testBaseUrl}/list-type`, (req, res, ctx) => {
+        const statusParams = req.url.searchParams.get('status');
+        postApiMockFn(statusParams);
+        return res(ctx.json({ id: 'foo' }));
+      }),
+      rest.put(`${testBaseUrl}/list-type/foo`, (req, res, ctx) => {
+        const statusParams = req.url.searchParams.get('status');
+        putApiMockFn(statusParams);
+        return res(ctx.status(201), ctx.json({ id: 'foo' }));
+      })
+    );
+  });
+
+  test('Content can be posted without specifying an id', async () => {
+    const data = await client.create<ContentType>({
+      endpoint: 'list-type',
+      content: {
+        title: 'title',
+        body: 'body',
+      },
+    });
+    expect(data).toEqual({ id: 'foo' });
+    // Confirm POST api was called
+    expect(postApiMockFn).toHaveBeenCalledTimes(1);
+  });
+  test('Draft content can be posted by specifying an id', async () => {
+    const data = await client.create<ContentType>({
+      endpoint: 'list-type',
+      content: {
+        title: 'title',
+        body: 'body',
+      },
+      isDraft: true,
+    });
+    expect(data).toEqual({ id: 'foo' });
+    // Confirm POST api was called
+    expect(postApiMockFn).toHaveBeenCalledTimes(1);
+    // Confirm that status=draft is specified for the query string
+    expect(postApiMockFn).toHaveBeenCalledWith('draft');
+  });
+  test('Content can be posted by specifying an id', async () => {
+    const data = await client.create<ContentType>({
+      endpoint: 'list-type',
+      contentId: 'foo',
+      content: {
+        title: 'title',
+        body: 'body',
+      },
+    });
+    expect(data).toEqual({ id: 'foo' });
+    // Confirm PUT api was called
+    expect(putApiMockFn).toHaveBeenCalledTimes(1);
+  });
+  test('Draft content can be posted by specifying an id', async () => {
+    const data = await client.create<ContentType>({
+      endpoint: 'list-type',
+      contentId: 'foo',
+      content: {
+        title: 'title',
+        body: 'body',
+      },
+      isDraft: true,
+    });
+    expect(data).toEqual({ id: 'foo' });
+    // Confirm PUT api was called
+    expect(putApiMockFn).toHaveBeenCalledTimes(1);
+    // Confirm that status=draft is specified for the query string
+    expect(putApiMockFn).toHaveBeenCalledWith('draft');
+  });
+
+  test('Returns an error message if `endpoint` is not specified', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    expect(client.create({})).rejects.toThrow(
+      new Error('endpoint is required')
+    );
+  });
 });
 describe('update', () => {
   test.todo('List format content can be updated');
