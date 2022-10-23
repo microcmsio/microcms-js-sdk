8a5f4996efc8a7faa7c9d4361d0625cff9741b41
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-10-23 20:29:46 +0900

    add: fetchClient

---
+++ src/lib/fetch.ts
@@ -0,0 +1,40 @@
+import crossFetch, { Headers as CrossFetchHeaders } from 'cross-fetch';
+import { Fetch } from 'src/types';
+
+export const resolveFetch = (customFetch?: Fetch): Fetch => {
+  let _fetch: Fetch;
+  if (customFetch) {
+    _fetch = customFetch;
+  } else if (typeof fetch === 'undefined') {
+    _fetch = (crossFetch as unknown) as Fetch;
+  } else {
+    _fetch = fetch;
+  }
+  return (...args) => _fetch(...args);
+};
+
+export const resolveHeadersConstructor = () => {
+  if (typeof Headers === 'undefined') {
+    return CrossFetchHeaders;
+  }
+
+  return Headers;
+};
+
+export const generateFetchClient = (
+  apiKey: string,
+  customFetch?: Fetch
+): Fetch => {
+  const fetch = resolveFetch(customFetch);
+  const HeadersConstructor = resolveHeadersConstructor();
+
+  return async (req, init) => {
+    const headers = new HeadersConstructor(init?.headers);
+
+    if (!headers.has('X-API-KEY')) {
+      headers.set('X-API-KEY', apiKey);
+    }
+
+    return fetch(req, { ...init, headers });
+  };
+};
