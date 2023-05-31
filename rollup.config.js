48228f81494e3650f098091484930110627a66e6
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2023-05-31 16:48:57 +0900

    update: node and terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 export default [
